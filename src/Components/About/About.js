46963ce75f8acb6e6b3e630d23f8431c2e972f19
Tom Doder <tdoder89@gmail.com>
2023-06-21 17:09:55 -0400

    added styling to about section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,6 +4,7 @@ import './About.css';
 const About = ( {about} ) => {
     return (
         <div className="about-section">
+            <section className='about-box'>
             <h1 className="about-header">About Me</h1>
             <p className="bio">- Hi, I'm Tom, a front-end software engineer from Washington DC with a passion for creating visually stunning and user-friendly digital experiences.
             
@@ -12,6 +13,7 @@ const About = ( {about} ) => {
             My interest in the web3 space has fueled my passion for technology, and I am always looking for new ways to learn and grow as a developer. I specialize in HTML, CSS, JavaScript, and React, and I believe in creating digital experiences that not only look great but are also highly functional and intuitive.
             
             I love collaborating with others and bringing ideas to life, and I'm excited to be a part of this ever-evolving field. Let's connect and create something amazing together!</p>
+            </section>
 </div>
     )
 }
